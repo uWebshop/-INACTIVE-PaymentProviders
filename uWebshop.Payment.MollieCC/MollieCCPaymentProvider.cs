1590f502a978f9a17240ac4b00ac6648e2f5806a
Webstudio Thom Hubers <info@thomhubers.nl>
2015-03-13 15:54:52 +0100

    Added Creditcard part of the new Mollie API. Manual install only, similar to old Mollie provider. Use Newtonsoft.Json 6.0.8.

---
+++ uWebshop.Payment.MollieCC/MollieCCPaymentProvider.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using uWebshop.Common;
+using uWebshop.Domain;
+using uWebshop.Domain.Helpers;
+using uWebshop.Domain.Interfaces;
+using Mollie.Api;
+
+namespace uWebshop.Payment.MollieCC
+{
+    public class MollieCCPaymentProvider : MollieCCPaymentBase, IPaymentProvider
+    {
+        PaymentTransactionMethod IPaymentProvider.GetParameterRenderMethod()
+        {
+            return PaymentTransactionMethod.QueryString;
+        }
+        public IEnumerable<PaymentProviderMethod> GetAllPaymentMethods(int id)
+        {
+            var paymentMethods = new List<PaymentProviderMethod>();
+
+            var paymentProvider = PaymentProvider.GetPaymentProvider(id);
+
+
+            if (paymentProvider == null)
+            {
+                Log.Instance.LogError("MollieCC PaymentProvider 'GetAllPaymentMethods' paymentProvider == null");
+
+                return paymentMethods;
+            }
+
+            var testMode = paymentProvider.TestMode;
+
+            var partnerId = paymentProvider.GetSetting("PartnerId");
+            
+            paymentMethods.Add(new PaymentProviderMethod
+            {
+                Id = "0",
+                Description = "Creditcard",
+                Title = "Creditcard",
+                Name = "Creditcard",
+                ProviderName = GetName()
+            });
+
+            return paymentMethods;
+        }
+    }
+}
---
+++ uWebshop.Payment.MollieCC/MollieCCPaymentResponseHandler.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Web;
+using Mollie.Api;
+using uWebshop.Domain;
+using uWebshop.Domain.Interfaces;
+using uWebshop.Domain.Helpers;
+using uWebshop.Common;
+
+namespace uWebshop.Payment.MollieCC
+{
+    public class MollieCCPaymentResponseHandler : MollieCCPaymentBase, IPaymentResponseHandler
+    {
+        public OrderInfo HandlePaymentResponse(PaymentProvider paymentProvider, OrderInfo orderInfo)
+        {
+            try
+            {
+                var orderId = HttpContext.Current.Request["order_id"];
+                orderInfo = OrderHelper.GetOrder(uniqueOrderId:new Guid(orderId));
+
+                Log.Instance.LogDebug("MollieCC Transaction Id: " + orderInfo.PaymentInfo.TransactionId);
+                
+                if (orderInfo == null)
+                {
+                    Log.Instance.LogError("MollieCC: Order Not Found For TransactionId: " + orderInfo.PaymentInfo.TransactionId);
+                    return null;
+                }
+
+                if (orderInfo.Paid != false)
+                {
+                    Log.Instance.LogDebug("MollieCC: Order already paid! TransactionId: " + orderInfo.PaymentInfo.TransactionId);
+                    return null;
+                }
+
+                var apiLiveKey = paymentProvider.GetSetting("ApiLiveKey");
+                var apiTestKey = paymentProvider.GetSetting("ApiTestKey");
+                bool testMode = paymentProvider.TestMode;
+
+                MollieClient mollieClient = new MollieClient();
+
+                if (testMode)
+                    mollieClient.setApiKey(apiTestKey);
+                else
+                    mollieClient.setApiKey(apiLiveKey);
+
+                PaymentStatus paymentStatus = mollieClient.GetStatus(orderInfo.PaymentInfo.TransactionId);
+
+                if (paymentStatus.status.Value.ToString() == "paid" || paymentStatus.status.Value.ToString() == "paidout")
+                {
+                    orderInfo.Paid = true;
+                    orderInfo.Status = OrderStatus.ReadyForDispatch;
+                }
+                else
+                {
+                    orderInfo.Paid = false;
+                    orderInfo.Status = OrderStatus.PaymentFailed;
+                    Log.Instance.LogError("MollieCCPaymentResponseHandler.HandlePaymentResponse NOT PAID: status:" + paymentStatus.status.Value.ToString() + " id: "+paymentStatus.id);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Instance.LogError("MollieCCPaymentResponseHandler.HandlePaymentResponse: " + ex);
+            }
+
+            return null;
+        }
+    }
+}
